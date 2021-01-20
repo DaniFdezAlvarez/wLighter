6f1bc4ce499cd8a9db2469f99e2c488ec085ead0
Dani Fdez <danifdezalvarez@gmail.com>
2021-01-20 22:06:23 +0100

    First version of code for #1. Need to test everything

--- wlighter/w_lighter.py
+++ wlighter/w_lighter.py
@@ -1,4 +1,5 @@
 import re
+import requests
 
 _PROP_DIRECT = "http://www.wikidata.org/prop/direct/"
 _PROP_INDIRECT = "http://www.wikidata.org/prop/"
@@ -51,6 +52,13 @@ _SHEXC_PREFIX = re.compile("(^| )PREFIX ")
 _SHEXC_PREFIX_SPEC = re.compile("([a-zA-Z]([a-zA-Z0-9\-]*[a-zA-Z0-9]+)?)? *: ")
 _SHEXC_NAMESPACE_SPEC = re.compile("<[^ <>]+>")
 
+_NO_LABEL = "(no label available)"
+_ARROW = " --> "
+_SEP_SPACES = "    "
+
+
+_ENTITIES_API_CALL = "https://www.wikidata.org/w/api.php?action=wbgetentities&props=labels&ids={}&languages={}" \
+                     ""
 class ShExCToyParser(AbstractParser):
     def _yield_prefix_namespace_paris_in_line(self, a_line):
         for a_match in re.finditer(_SHEXC_PREFIX, a_line):
@@ -67,12 +75,11 @@ class ShExCToyParser(AbstractParser):
 
 class WLighter(object):
 
-    def __init__(self, raw_input, file_input, format, languages, wikidata_alt_api):
+    def __init__(self, raw_input, file_input, format, languages):
         self._raw_input = raw_input
         self._file_input = file_input
         self._format = format
         self._languages = languages
-        self._wikidata_alt_api = wikidata_alt_api
 
         self._parser = self._choose_parser()
 
@@ -89,25 +96,138 @@ class WLighter(object):
         self._prop_direct_prefixed_pattern = None
         self._prop_indirect_full_pattern = None
         self._prop_indirect_prefixed_pattern = None
+        self._languages_for_api = self._build_languages_for_api()
+
+        self._cache = {}
+
+        self._out_stream = None
+        self._accumulated_result = None
 
 
 
-    def annotate_entities(self):
-        pass
-
-    def annotate_properties(self):
-        pass
-
-    def annotate_all(self):
-        self._set_up()
-
+    def annotate_entities(self, out_file, string_return):
+        self._set_up(out_file, string_return)
         for a_line in self._parser.yield_lines():
+            comments = None
+            entity_mentions = self._look_for_entity_mentions(a_line)
+            if len(entity_mentions) != 0:
+                comments += self._turn_entities_into_comments(entity_mentions)
+            self._write_line(line=a_line,
+                             comments=comments)
+        self._tear_down()
+        return self._return_result(string_return)
+
+    def annotate_properties(self, out_file, string_return):
+        self._set_up(out_file, string_return)
+        for a_line in self._parser.yield_lines():
+            comments = None
+            prop_mentions = self._look_for_prop_mentions(a_line)
+            if len(prop_mentions) != 0:
+                comments = self._turn_entities_into_comments(prop_mentions)
+            self._write_line(line=a_line,
+                             comments=comments)
+        self._tear_down()
+        return self._return_result(string_return)
+
+
+    def annotate_all(self, out_file, string_return):
+        self._set_up(out_file, string_return)
+        for a_line in self._parser.yield_lines():
+            comments = None
             entity_mentions = self._look_for_entity_mentions(a_line)
             prop_mentions = self._look_for_prop_mentions(a_line)
+            if len(entity_mentions) != 0 or len(prop_mentions) != 0:
+                comments = self._turn_entities_into_comments(prop_mentions)  # first the props
+                comments += self._turn_entities_into_comments(entity_mentions)
+            self._write_line(line=a_line,
+                             comments=comments)
+        self._tear_down()
+        return self._return_result(string_return)
 
-    def _set_up(self):
+    def _write_line(self, line, comments):
+        target_line = self._add_comments_to_line(line, comments)
+        if self._accumulated_result is not None:
+            self._accumulated_result.append(target_line)
+        if self._out_stream is not None:
+            self._out_stream.write(target_line + "\n")
+
+    def _add_comments_to_line(self, line, comments):
+        return line + _SEP_SPACES + "# " + ";".join(comments)
+
+    def _tear_down(self):
+        if self._out_stream is not None:
+            self._out_stream.close()
+        self._out_stream = None
+
+    def _return_result(self, string_return):
+        if string_return:
+            return "\n".join(self._accumulated_result)
+
+
+    def _build_languages_for_api(self):
+
+        if len(self._languages) == 0:
+            return "en"
+        result = "|".join(self._languages)
+        if "en" in self._languages:
+            return result
+        else:
+            return result + "|en"
+
+
+    def _turn_entities_into_comments(self, entity_mentions):
+        if len(entity_mentions) == 0:
+            return []
+        result = []
+        for a_mention in entity_mentions:
+            label = self._solve_entity_label(a_mention)
+            result.append(self._turn_id_into_comment(id_wiki=a_mention,
+                                                     label=label))
+
+
+    def _turn_id_into_comment(self, id_wiki, label):
+        return "{} {} {}".format(id_wiki,
+                                 _ARROW,
+                                 label)
+
+    def _solve_entity_label(self, entity):
+        if entity in self._cache:
+            return self._cache[entity]
+        else:
+            result = self._entity_api_call(entity)
+            self._cache[entity] = result
+            return result
+
+
+    def _entity_api_call(self, entity):
+        response = requests.get(_ENTITIES_API_CALL.format(entity,
+                                                          self._languages_for_api)).json()
+        labels = response["entities"][entity]["labels"]
+        for a_language in self._languages:
+            if a_language in labels:
+                return labels[a_language]["value"]
+        if "en" in labels:
+            return labels["en"]["value"]
+        return _NO_LABEL
+
+
+    def _set_up(self, out_file, string_return):
+        if self._file_input is not None and self._file_input == out_file:
+            raise ValueError("Please, do not use the same disk path as input and output at a time")
         self._look_for_namespaces()
         self._compile_patterns()
+        if string_return:
+            self._accumulated_result = []
+        else:
+            self._accumulated_result = None
+        if out_file is not None:
+            self._reset_file(out_file)
+            self._out_stream = open(out_file, "wa")
+
+    def _reset_file(self, out_file):
+        with open(out_file, "w") as out_stream:
+            out_stream.write("")
+
 
     def _look_for_entity_mentions(self, a_line):
         full_mentions = re.findall(self._entity_full_pattern, a_line)
@@ -117,13 +237,26 @@ class WLighter(object):
         prefixed_mentions = re.findall(self._entity_prefixed_pattern, a_line)
         if len(full_mentions) != 0:
             prefixed_mentions = self._extract_id_from_prefixed_uris(id_type="Q",
-                                                                     mentions_list=full_mentions)
+                                                                    mentions_list=full_mentions)
+
+        return set(full_mentions + prefixed_mentions)
+
+    def _look_for_prop_mentions(self, a_line):
+        full_mentions = re.findall(self._prop_direct_full_pattern, a_line)
+        full_mentions += re.findall(self._prop_indirect_full_pattern, a_line)
+        if len(full_mentions) != 0:
+            full_mentions = self._extract_id_from_full_uris(id_type="P",
+                                                            mentions_list=full_mentions)
+
+        prefixed_mentions = re.findall(self._prop_direct_prefixed_pattern, a_line)
+        prefixed_mentions += re.findall(self._prop_indirect_prefixed_pattern, a_line)
+        if len(full_mentions) != 0:
+            prefixed_mentions = self._extract_id_from_prefixed_uris(id_type="P",
+                                                                    mentions_list=full_mentions)
+
+        return set(full_mentions + prefixed_mentions)
 
 
-        # todo : continue here
-
-    def _look_for_entity_mentions(self, a_line):
-        pass
 
     def _extract_id_from_full_uris(self, id_type, mentions_list):
         result = []
